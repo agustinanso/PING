a627ae0bb1948c7b2096d72fdf6dc05b19b3f254
agustin.anso <agustin.anso97@gmail.com>
2023-06-08 18:26:00 -0300

    Terminado

--- projectoPing/main.js
+++ projectoPing/main.js
@@ -1,19 +1,28 @@
 const formulario = document.getElementById("formulario-1");
 
 const btnEnviar = document.querySelector("#btnEnviar");
+const correo = document.querySelector("#correo");
+const alertRoja = document.querySelector("#alertRoja");
+const alertVerde = document.querySelector("#alertVerde");
 
 formulario.addEventListener("submit", (e) => {
     e.preventDefault();
+
+    const expresionRegular = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+
     const dato = {
         correo: formulario.correo.value,
     };
 
-    if (dato.correo === "") {
-        console.log("No hay nada escrito");
-    }
-
-    if (dato.correo.length <= 4) {
-        console.log("correo invalido");
+    if (!expresionRegular.test(dato.correo)) {
+        correo.classList.add("border-red-500");
+        alertRoja.classList.remove("hidden");
+        return;
+    } else {
+        alertRoja.classList.add("hidden");
+        alertVerde.classList.remove("hidden");
+        correo.classList.remove("border-red-500");
+        correo.classList.add("border-emerald-500");
         return;
     }
 });
